e0643f49b7bcb3fccf2ec3d2491631c6e84ef7be
kenkuma-daz <80566958+kenkuma-daz@users.noreply.github.com>
2021-05-16 22:59:34 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -52,10 +52,24 @@ namespace itembox.sprite {
 namespace itembox.listeners {
     export class Listener {
         _kind: number;
-        _handler: () => void;
+        _focus: () => void;
+        _action: () => void;
+        _blur: () => void;
 
         constructor() {
+            this._focus = null;
+            this._action = null;
+            this._blur = null;
         }
+    }
+
+    export function findListenerByKind(listeners: Listener[], itemKind: number) {
+        let listener = listeners.find((listener: itembox.listeners.Listener, index: number) => {
+            return listener._kind == itemKind;
+        });
+        if( listener == undefined )
+            return null;
+        return listener;
     }
 }
 
@@ -93,7 +107,8 @@ namespace itembox.items {
     }
 
     export class ItemBox {
-        _selected: number;
+        _previous: number;
+        _current: number;
         _focus: Sprite;
 
         _items: Item[];
@@ -101,7 +116,8 @@ namespace itembox.items {
 
 
         constructor() {
-            this._selected = 0;
+            this._previous = -1;
+            this._current = 0;
             this._focus = itembox.sprite.createFocus();
             this._focus.z = 1;
             this._focus.setFlag(SpriteFlag.RelativeToCamera, true);
@@ -113,16 +129,6 @@ namespace itembox.items {
 
         attach(listeners : itembox.listeners.Listener[]) {
             this._listeners = listeners;
-        }
-
-
-        _findListenerByKind(itemKind: number) {
-            let _listener = this._listeners.find((listener: itembox.listeners.Listener, index: number) => {
-                return listener._kind == itemKind;
-            });
-            if( _listener == undefined )
-                return null;
-            return _listener;
         }
         
         //% block="ItemBox $this(itemBox) add $itemKind $img"    
@@ -136,7 +142,10 @@ namespace itembox.items {
             if( found )
                 return;
 
-            this._findListenerByKind(itemKind)
+            // this._findListenerByKind(itemKind)
+
+            console.log("add() kind:" + itemKind);
+
 
             let sprite = sprites.create(img, SpriteKind.ItemBox);
             let item = new Item(itemKind, sprite);
@@ -151,43 +160,39 @@ namespace itembox.items {
             this._updateFocus();
         }
 
-
-
-        //% block="action $this(itemBox)"    
-        action() {
-            let item = this._items[this._selected];
-            if( !item )
-                return;
-            let listener = this._findListenerByKind(item._kind);
-            if( !listener )
-                return;
-            listener._handler();
-
-        }
-
         //% block="ItemBox $this(itemBox) selected $itemKind "    
         //% itemKind.shadow="item_kind_enum_shim"
         isSelected(itemKind:number) : boolean {
-            let item = this._items[this._selected];
+            let item = this._items[this._current];
             return item._kind == itemKind;
+        }
+
+        //% block="action $this(itemBox)"    
+        action() {
+            this._onAction(this._current);
         }
 
         //% block="next $this(itemBox)"    
         next() {
-            this._selected += 1;
-            if( this._selected >= this._items.length )
-                this._selected = 0;
+            this._current += 1;
+            if( this._current >= this._items.length )
+                this._current = 0;
             this._updateFocus();
+            this._onBlur(this._previous);
+            this._onFocus(this._current);
+            this._previous = this._current;
         }
 
         //% block="prev $this(itemBox)"    
         prev() {
-            this._selected -= 1;
-            if( this._selected < 0)
-                this._selected = this._items.length - 1;
+            this._current -= 1;
+            if( this._current < 0)
+                this._current = this._items.length - 1;
             this._updateFocus();
+            this._onBlur(this._previous);
+            this._onFocus(this._current);
+            this._previous = this._current;
         }
-        
 
         _calcPos(sprite: Sprite, index: number) {
             let iconSize = sprite.image.width;
@@ -196,7 +201,7 @@ namespace itembox.items {
             let itemCenter = (this._items.length * iconSize) / 2.0;
             let start = center - itemCenter;
             let pos = {
-                x: index * iconSize + start,
+                x: index * iconSize + start + iconSize * 0.5,
                 y: scene.screenHeight() - iconSize
             }
 
@@ -204,8 +209,33 @@ namespace itembox.items {
         }
 
         _updateFocus() {
-            let pos = this._calcPos(this._focus, this._selected);
+            let pos = this._calcPos(this._focus, this._current);
             this._focus.setPosition(pos.x-1, pos.y);
+        }
+
+        _onFocus(index: number) {
+            let listener = this._findListenerByIndex(index);
+            if( listener && listener._focus)
+                listener._focus();
+        }
+
+        _onAction(index: number) {
+            let listener = this._findListenerByIndex(index);
+            if( listener && listener._action)
+                listener._action();
+        }
+
+        _onBlur(index: number) {
+            let listener = this._findListenerByIndex(index);
+            if( listener && listener._blur)
+                listener._blur();
+        }
+
+        _findListenerByIndex(index: number) : itembox.listeners.Listener {
+            if( index == -1)
+                return null;
+            let item = this._items[index];
+            return itembox.listeners.findListenerByKind(this._listeners, item._kind);
         }
 
         debugPrint() {
@@ -214,13 +244,10 @@ namespace itembox.items {
             }
 
             console.log("this._items.length:" + this._items.length);
-            console.log("this._selected:" + this._selected);
+            console.log("this._current:" + this._current);
         }
 
     }
-
-
-
 }
 
 
@@ -257,15 +284,43 @@ namespace itembox.util {
         return arg;
     }
 
+    //% blockId=on_focus_with_item_kind
+    //% block="on $itemKind focus event"
+    //% itemKind.shadow="item_kind_enum_shim"
+    export function onFocus(itemKind: number, handler: () => void) {
+        let listener = itembox.listeners.findListenerByKind(_listeners, itemKind);
+        if( !listener ) {
+            listener = new itembox.listeners.Listener();
+            listener._kind = itemKind;
+            _listeners.push(listener);
+        }
+        listener._focus = handler;
+    }
+
     //% blockId=on_event_with_item_kind
-    //% block="on event $itemKind executed 2"
+    //% block="on $itemKind action event"
     //% itemKind.shadow="item_kind_enum_shim"
     export function onEvent(itemKind: number, handler: () => void) {
-        let listener = new itembox.listeners.Listener();
-        listener._kind = itemKind;
-        listener._handler = handler;
-        _listeners.push(listener);
+        let listener = itembox.listeners.findListenerByKind(_listeners, itemKind);
+        if( !listener ) {
+            console.log("add listener kind:" + itemKind);
+            listener = new itembox.listeners.Listener();
+            listener._kind = itemKind;
+            _listeners.push(listener);
+        }
+        listener._action = handler;
+    }
+
+    //% blockId=on_blur_with_item_kind
+    //% block="on $itemKind blur event"
+    //% itemKind.shadow="item_kind_enum_shim"
+    export function onBlur(itemKind: number, handler: () => void) {
+        let listener = itembox.listeners.findListenerByKind(_listeners, itemKind);
+        if( !listener ) {
+            listener = new itembox.listeners.Listener();
+            listener._kind = itemKind;
+            _listeners.push(listener);
+        }
+        listener._blur = handler;
     }
 }
-
-
